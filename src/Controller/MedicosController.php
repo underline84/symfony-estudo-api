70a9fd1cd57e5abec88bbe84237dddb8d3cfa207
underline84 <evanfatu@hotmail.com>
2021-10-12 22:20:40 -0300

    first commit

---
+++ src/Controller/MedicosController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Medico;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MedicosController
+{
+    /**
+     * @Route("/medicos", methods={"POST"})
+     */
+    public function novo(Request $request) : Response
+    {
+       $corpoRequisicao = $request->getContent();
+       $dadoEmJson = json_decode($corpoRequisicao);
+
+       $medico = new Medico();
+       $medico->crm = $dadoEmJson->crm;
+       $medico->nome = $dadoEmJson->nome;
+
+       return new JsonResponse($medico);
+    }
+}
---
+++ src/Entity/Medico.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Entity;
+
+class Medico
+{
+    public $crm;
+    public $nome;
+    
+}
